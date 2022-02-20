44d0fb6a8c93ce8e0e0621dd7393b83e2eb8696f
Bhargav Bachina <bhargavbachina@Bhargavs-MacBook-Pro.local>
2022-02-20 15:42:25 -0600

    changes

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '^/api': {
-          target: 'http://localhost:3080',
+          target: 'http://mevn-api:3080',
           changeOrigin: true
         },
       }
